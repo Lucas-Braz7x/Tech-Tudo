ddaa638f156d978b38a8996d88a8a9778afd68fa
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-07-16 21:23:52 -0300

    Criando função para salvar o email no localStorage

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('form');
+
+class Clientes{
+  constructor(nome, email){
+    this.nome = nome;
+    this.email = email;
+  }
+}
+
+class BancoDados{
+  
+  constructor(){
+    let id = localStorage.getItem('id');
+
+    if(id === null){
+      localStorage.setItem('id', 0);
+    }
+  }
+
+  getProximoId(){
+    let proximoId = localStorage.getItem('id');
+    return parseInt(proximoId) + 1;
+  }
+  
+  gravar(clienteAtual){
+    //localStorage.setItem('despesa', JSON.stringify(despesaAtual));
+    let id =this.getProximoId();
+
+    localStorage.setItem(id, JSON.stringify(clienteAtual));
+
+    localStorage.setItem('id', id);
+  }
+}
+
+let bancoDados = new BancoDados();
+
+form.addEventListener('submit', event =>{
+  event.preventDefault();
+  let name = document.getElementById('name');
+  let email = document.getElementById('email');
+  
+  let cliente = new Clientes(
+    name.value,
+    email.value
+  )
+
+ bancoDados.gravar(cliente)
+})
